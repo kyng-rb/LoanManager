cc867c90f847078b83cdb5de8b935bc68962303d
Reynaldo <rodriguezreynaldo1698@outlook.com>
2022-10-10 19:22:48 -0600

    Updated swagger rooth path

--- src/LoanManager.Presentation.API/Extensions/SwaggerExtensions.cs
+++ src/LoanManager.Presentation.API/Extensions/SwaggerExtensions.cs
@@ -46,6 +46,10 @@ public static class SwaggerExtensions
             return;
 
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
     }
 }
